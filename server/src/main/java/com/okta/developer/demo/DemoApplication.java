aacdca88b647c34ce9179ad1e614c4fa199cfcd3
Matt Raible <matt.raible@okta.com>
2018-11-02 05:47:56 -0600

    Tabs > Spaces

--- server/src/main/java/com/okta/developer/demo/DemoApplication.java
+++ server/src/main/java/com/okta/developer/demo/DemoApplication.java
@@ -18,22 +18,22 @@ import java.util.stream.Stream;
 @SpringBootApplication
 public class DemoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
 
-	@Bean
-	ApplicationRunner init(CarRepository repository) {
-		return args -> {
-			Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
-					"AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
-				Car car = new Car();
-				car.setName(name);
-				repository.save(car);
-			});
-			repository.findAll().forEach(System.out::println);
-		};
-	}
+    @Bean
+    ApplicationRunner init(CarRepository repository) {
+        return args -> {
+            Stream.of("Ferrari", "Jaguar", "Porsche", "Lamborghini", "Bugatti",
+                    "AMC Gremlin", "Triumph Stag", "Ford Pinto", "Yugo GV").forEach(name -> {
+                Car car = new Car();
+                car.setName(name);
+                repository.save(car);
+            });
+            repository.findAll().forEach(System.out::println);
+        };
+    }
 
     @Bean
     public FilterRegistrationBean<CorsFilter> simpleCorsFilter() {
